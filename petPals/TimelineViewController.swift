711b71904c30cedb19168f27407b41b9779c21d3
Shaun Chua <shon3005@gmail.com>
2015-10-31 22:32:23 -0400

    sent first file to parse

--- petPals/TimelineViewController.swift
+++ petPals/TimelineViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class TimelineViewController: UIViewController {
   
@@ -20,9 +21,17 @@ class TimelineViewController: UIViewController {
 
   func takePhoto() {
     // instantiate photo taking class, provide callback for when photo  is selected
-    let photoTakingHelper = PhotoTakingHelper(viewController: self.tabBarController!) { (image: UIImage?) in
-      // don't do anything, yet...
-    }
+    photoTakingHelper = PhotoTakingHelper(viewController: self.tabBarController!, callback: { (image: UIImage?) in
+      if let image = image {
+        let imageData = UIImageJPEGRepresentation(image, 0.8)
+        let imageFile = PFFile(data: imageData!)
+        try! imageFile.save()
+        
+        let post = PFObject(className: "Post")
+        post["imageFile"] = imageFile
+        try! post.save()
+      }
+    })
   }
 }
 
